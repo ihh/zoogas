ffb53c256792b10101f0d1e13dcf80df620820db
Ian Holmes <yam@emin.local>
2009-10-10 14:50:38 -0700

    I'm dreaming of a regexp language

--- Particle.java
+++ Particle.java
@@ -7,13 +7,14 @@ import java.io.*;
 
 
 // Here is a hypothetical syntax for regexp-based pattern generators:
-//  A B C D E
+//  A B C D P V
 // where
 //  A is a regexp that must globally match the old source state
 //  B is a regexp that must globally match the old target state
 //  C is a string that will expand to the new source state
 //  D is a string that will expand to the new target state
-//  E is a numeric constant that is the probability of the rule
+//  P is a numeric constant that is the probability of the rule
+//  V is a verb describing the action being carried out by the source when this rule fires (no whitespace)
 
 // The following "special variables" will be expanded in {A,B,C,D} as appropriate:
 //  $1,$2,$3... => groups in A and B regexps (c.f. Perl)
@@ -30,7 +31,7 @@ import java.io.*;
 
 // Patterns such as this are to be transmitted in a "Particle definition" packet with the following structure:
 // NAMES & COLORS (one per line, format "NAME R G B", describing appearances of Particles to which this definition packet applies)
-// RULES (one per line, format "A B C D E")
+// RULES (one per line, format "A B C D P V")
 
 // The class (ParticleDefinition? ParticleTemplate?) encapsulating these definition data
 // is a regexp-based generator for Particles and their production rules.
@@ -39,7 +40,7 @@ import java.io.*;
 // Particle class, encapsulating the behavior, appearance & summary statistics of a given CA state
 public class Particle {
     // appearance
-    public String name = null;  // this string uniquely identifys this Particle
+    public String name = null;  // noun uniquely identifying this Particle (no whitespace)
     public Color color = null;
 
     // behavior
